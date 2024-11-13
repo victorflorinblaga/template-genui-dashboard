55cfc40825eafeb499307c9d7a40a5ef7b80f72e
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-13 09:08:36 +0100

    -

---
+++ template/src/app/dashboards/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExamplePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Example Page</h1>
+            <p>This is an example React page.</p>
+        </div>
+    );
+};
+
+export default ExamplePage;
